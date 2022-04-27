08f100b41d628643ac796fb06a50eec515ce9211
Isaque Gabriel <72178423+IsaqueGabriel1@users.noreply.github.com>
2022-04-27 13:49:56 -0300

    Update MediaComPonteiros.cpp

--- MediaComPonteiros.cpp
+++ MediaComPonteiros.cpp
@@ -3,12 +3,11 @@
 
 int main(){
 	int *n;
-	int i, quant_notas, soma, media, T;
+	int i = 0, quant_notas = 0, soma = 0, media = 0;
 	
 	printf("Digite a qnt de notas: ");
 	scanf("%d", &quant_notas);
 	
-	T = quant_notas;
 	n = (int *)(malloc(quant_notas * sizeof(int)));
 	
 	
@@ -18,13 +17,14 @@ int main(){
 		system("pause");
 		exit(1);	
 	}
-	
+	printf("%d", quant_notas);
 	for(i=0; i<quant_notas; i++){
 		printf("\nDigite a nota %d: ", i);
 		scanf("%d", &n[i]);
 		soma += n[i];
 	}
-	media = soma / T;
+	printf("%d", soma);
+	media = soma / quant_notas;
 	printf("\nA media do aluno foi de: %d pontos", media);
 	
 	free(n);
